099ce4ae31e658ae6d5e9fdd48e91c2244716523
williamabreu <williamsantos_00@hotmail.com>
2018-10-15 00:42:20 -0300

    Separado main da classe

--- ga/genetic_algorithm.py
+++ ga/genetic_algorithm.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 class GeneticAlgorithm:
@@ -83,60 +82,3 @@ class GeneticAlgorithm:
             if np.random.rand() < self.__MUTATION_RATE:
                 child[point] = 1 if child[point] == 0 else 0
         return child
-
-def main(ga):
-    population = np.random.randint(2, size=(ga.get_population_size(), ga.get_bit_len()))  # initialize the pop DNA
-
-    plt.ion()  # something about plotting
-    x = np.linspace(*ga.get_x_bounds(), 200)
-    plt.plot(x, ga.f(x))
-
-    for _ in range(ga.get_num_generations()-1):
-        F_values = ga.f(ga.binary_to_float(population))  # compute function value by extracting DNA
-
-        # something about plotting
-        sca = plt.scatter(ga.binary_to_float(population), F_values, s=200, lw=0, c='red', alpha=0.5)
-        plt.pause(0.1)
-        sca.remove()
-
-        # GA part (evolution)
-        fitness = ga.calculate_fitness(F_values)
-        print("Most fitted DNA: ", population[np.argmax(fitness), :])
-        population = ga.select(population, fitness)
-        pop_copy = population.copy()
-        for parent in population:
-            child = ga.crossover(parent, pop_copy)
-            child = ga.mutate(child)
-            parent[:] = child  # parent is replaced by its child
-    
-    F_values = ga.f(ga.binary_to_float(population))  # compute function value by extracting DNA
-
-    # something about plotting
-    sca = plt.scatter(ga.binary_to_float(population), F_values, s=200, lw=0, c='red', alpha=0.5)
-
-    # GA part (evolution)
-    fitness = ga.calculate_fitness(F_values)
-    print("Most fitted DNA: ", population[np.argmax(fitness), :])
-    population = ga.select(population, fitness)
-    pop_copy = population.copy()
-    for parent in population:
-        child = ga.crossover(parent, pop_copy)
-        child = ga.mutate(child)
-        parent[:] = child  # parent is replaced by its child
-
-    plt.ioff()
-    plt.show()
-
-
-if __name__ == '__main__':
-    ga = GeneticAlgorithm(
-        DNA_LEN=8,
-        POP_SIZE=30,
-        CROSS_RATE=0.7,
-        MUTATION_RATE=0.01,
-        N_GENERATIONS=20,
-        X_BOUND=[-10, 10],
-        FUNCTION=lambda x: np.sin(10*x)*x + np.cos(2*x)*x
-    )
-
-    main(ga)
